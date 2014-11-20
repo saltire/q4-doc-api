7ada9f1d998ef3e7d15be4a8a7bac1ff58f985f1
saltire sable <x@saltiresable.com>
2014-11-20 17:42:43 -0500

    Use separate schemas for each content type

--- actions.js
+++ actions.js
@@ -1,3 +1,18 @@
+// the total number of documents
 module.exports.count = function (data) {
     return {total: data.length};
+};
+
+// the number of documents in each year
+module.exports.years = function (data) {
+    var years = {};
+    for (var i = 0; i < data.length; i++) {
+        var year = data[i].getYear();
+
+        if (!(year in years)) {
+            years[year] = 0;
+        }
+        years[year]++;
+    }
+    return {years: years};
 };
--- models.js
+++ models.js
@@ -1,62 +1,87 @@
 var mongoose = require('mongoose');
 
 
-var Any = new mongoose.Schema();
-
 // valid content types existing in the database
 var contentTypes = {
     contentAssets: {
-        model: mongoose.model('ContentAsset', Any, 'ContentAssets'),
         query: [
             {'Q4Dto.FileType': 'PDF'}
-        ]
+        ],
+        getYear: function () {
+            return new Date(this.get('Q4Dto.ContentAssetDate')).getFullYear();
+        }
     },
 
     events: {
-        model: mongoose.model('Event', Any, 'Events'),
         query: [
             {'Q4Dto.EventPresentation.DocumentFileType': 'PDF'},
             {'Q4Dto.EventPressRelease.DocumentFileType': 'PDF'},
             {'Q4Dto.DocumentFileType': 'PDF'},
             {'Q4Dto.Attachments': {$not: {$size: 0}}}
-        ]
+        ],
+        getYear: function () {
+            return new Date(this.get('Q4Dto.StartDate')).getFullYear();
+        }
     },
 
     financialReports: {
-        model: mongoose.model('FinancialReport', Any, 'FinancialReports'),
         query: [
             {'Q4Dto.Documents': {$elemMatch: {DocumentFileType: 'PDF'}}}
-        ]
+        ],
+        getYear: function () {
+            return this.get('Q4Dto.ReportYear');
+        }
     },
 
     presentations: {
-        model: mongoose.model('Presentation', Any, 'Presentations'),
         query: [
             {'Q4Dto.DocumentFileType': 'PDF'}
-        ]
+        ],
+        getYear: function () {
+            return new Date(this.get('Q4Dto.PresentationDate')).getFullYear();
+        }
     },
 
     pressReleases: {
-        model: mongoose.model('PressRelease', Any, 'PressReleases'),
         query: [
             {'Q4Dto.DocumentFileType': 'PDF'}
-        ]
+        ],
+        getYear: function () {
+            return new Date(this.get('Q4Dto.PressReleaseDate')).getFullYear();
+        }
     }
 }
 
 
-// check if the given content type is one of the above
+// build a schema and register a model for each content type
+for (contentType in contentTypes) {
+    var schema = new mongoose.Schema();
+    schema.static({
+        getDocs: function () {
+            return this.model(this.modelName).find({
+                $or: contentTypes[this.modelName].query
+            });
+        }
+    }).method({
+        getYear: contentTypes[contentType].getYear
+    });
+    mongoose.model(contentType, schema, contentType.charAt(0).toUpperCase() + contentType.slice(1));
+}
+
+
+// check if content type exists
 module.exports.validContentType = function (contentType) {
     return contentType in contentTypes;
 };
 
+
 // fetch data from the database for this client and content type
 module.exports.getData = function (req, callback) {
-    contentTypes[req.contentType].model
-        .find({
+    mongoose.model(req.contentType)
+        .getDocs()
+        .and({
             SiteName: req.sitename
         })
-        .or(contentTypes[req.contentType].query)
         .and(req.query.tag ? [{
             'Q4Dto.TagsList': {
                 $in: [].concat(req.query.tag)
