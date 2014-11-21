88634733847fd71f790703a98ce58a31d7dd737c
saltire sable <x@saltiresable.com>
2014-11-20 19:31:09 -0500

    Add year filters, refactor queries Year filter is still commented out of the query, since the test DB is currently storing dates as strings.

--- models.js
+++ models.js
@@ -1,70 +1,93 @@
 var mongoose = require('mongoose');
 
 
+var defaultLimit = 20;
+
+
 // valid content types existing in the database
 var contentTypes = {
     contentAssets: {
-        query: [
-            {'Q4Dto.FileType': 'PDF'}
-        ],
-        getYear: function () {
-            return new Date(this.get('Q4Dto.ContentAssetDate')).getFullYear();
+        docQuery: {'Q4Dto.FileType': 'PDF'},
+        yearQuery: function (year) {
+            return {'Q4Dto.ContentAssetDate': {
+                $gte: new Date(year, 1, 1),
+                $lt: new Date(year + 1, 1, 1)
+            }};
+        },
+        methods: {
+            getYear: function () {
+                return new Date(this.get('Q4Dto.ContentAssetDate')).getFullYear();
+            }
         }
     },
 
     events: {
-        query: [
+        docQuery: {$or: [
             {'Q4Dto.EventPresentation.DocumentFileType': 'PDF'},
             {'Q4Dto.EventPressRelease.DocumentFileType': 'PDF'},
             {'Q4Dto.DocumentFileType': 'PDF'},
             {'Q4Dto.Attachments': {$not: {$size: 0}}}
-        ],
-        getYear: function () {
-            return new Date(this.get('Q4Dto.StartDate')).getFullYear();
+        ]},
+        yearQuery: function (year) {
+            return {'Q4Dto.StartDate': {
+                $gte: new Date(year, 1, 1),
+                $lt: new Date(year + 1, 1, 1)
+            }};
+        },
+        methods: {
+            getYear: function () {
+                return new Date(this.get('Q4Dto.StartDate')).getFullYear();
+            }
         }
     },
 
     financialReports: {
-        query: [
-            {'Q4Dto.Documents': {$elemMatch: {DocumentFileType: 'PDF'}}}
-        ],
-        getYear: function () {
-            return this.get('Q4Dto.ReportYear');
+        docQuery: {'Q4Dto.Documents': {$elemMatch: {DocumentFileType: 'PDF'}}},
+        yearQuery: function (year) {
+            return {'Q4Dto.ReportYear': year}
+        },
+        methods: {
+            getYear: function () {
+                return this.get('Q4Dto.ReportYear');
+            }
         }
     },
 
     presentations: {
-        query: [
-            {'Q4Dto.DocumentFileType': 'PDF'}
-        ],
-        getYear: function () {
-            return new Date(this.get('Q4Dto.PresentationDate')).getFullYear();
+        docQuery: {'Q4Dto.DocumentFileType': 'PDF'},
+        yearQuery: function (year) {
+            return {'Q4Dto.PresentationDate': {
+                $gte: new Date(year, 1, 1),
+                $lt: new Date(year + 1, 1, 1)
+            }};
+        },
+        methods: {
+            getYear: function () {
+                return new Date(this.get('Q4Dto.PresentationDate')).getFullYear();
+            }
         }
     },
 
     pressReleases: {
-        query: [
-            {'Q4Dto.DocumentFileType': 'PDF'}
-        ],
-        getYear: function () {
-            return new Date(this.get('Q4Dto.PressReleaseDate')).getFullYear();
+        docQuery: {'Q4Dto.DocumentFileType': 'PDF'},
+        yearQuery: function (year) {
+            return {'Q4Dto.PressReleaseDate': {
+                $gte: new Date(year, 1, 1),
+                $lt: new Date(year + 1, 1, 1)
+            }};
+        },
+        methods: {
+            getYear: function () {
+                return new Date(this.get('Q4Dto.PressReleaseDate')).getFullYear();
+            }
         }
     }
 }
 
-
 // build a schema and register a model for each content type
 for (contentType in contentTypes) {
     var schema = new mongoose.Schema();
-    schema.static({
-        getDocs: function () {
-            return this.model(this.modelName).find({
-                $or: contentTypes[this.modelName].query
-            });
-        }
-    }).method({
-        getYear: contentTypes[contentType].getYear
-    });
+    schema.method(contentTypes[contentType].methods);
     mongoose.model(contentType, schema, contentType.charAt(0).toUpperCase() + contentType.slice(1));
 }
 
@@ -74,26 +97,21 @@ module.exports.validContentType = function (contentType) {
     return contentType in contentTypes;
 };
 
-
 // fetch data from the database for this client and content type
 module.exports.getData = function (req, callback) {
     mongoose.model(req.contentType)
-        .getDocs()
-        .and({
-            SiteName: req.sitename
-        })
-        .and(req.query.tag ? [{
-            'Q4Dto.TagsList': {
-                $in: [].concat(req.query.tag)
-            }
-        }] : null)
-        .and(req.query.year ? [{
-            'Q4Dto.StartDate': {
-                $gte: new Date(req.query.year, 1, 1),
-                $lt: new Date(req.query.year + 1, 1, 1)
-            }
-        }] : null)
+        .find(contentTypes[req.contentType].docQuery)
+        .and([
+            {SiteName: req.sitename},
+            req.query.tag ? {
+                'Q4Dto.TagsList': {$in: [].concat(req.query.tag)}
+            } : {},
+            //req.query.year ? contentTypes[req.contentType].yearQuery(req.query.year) : {},
+            req.contentType == 'contentAssets' && req.query.type ? {
+                'Q4Dto.Type': req.query.type
+            } : {}
+        ])
         .skip(req.query.skip || 0)
-        .limit(req.query.limit || 20)
+        .limit(req.query.limit || defaultLimit)
         .exec(callback);
 };
